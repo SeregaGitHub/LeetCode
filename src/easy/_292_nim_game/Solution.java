d0bba6b35ad4862ee8cd14f66ff1802e00731ff6
Sergei Kraev <serega.kraev1@yandex.ru>
2026-02-17 11:56:36 +0300

    292

---
+++ src/easy/_292_nim_game/Solution.java
@@ -0,0 +1,12 @@
+package easy._292_nim_game;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean canWinNim(int n) {
+
+        return (n % 4 != 0);
+    }
+}
